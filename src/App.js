6eeb75d6392083056a4f82f6045423fdc6c94e1c
Przemek <krakowskiprzem@gmail.com>
2022-07-25 20:34:30 +0200

    template route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div>MENU</div>
+        <Routes>
+          <Route path="/" element={<div>LIST</div>} />
+          <Route path="/preview" element={<div>PODGLAD</div>} />
+          <Route path="/admin/create" element={<div>UTWORZ ADMINA</div>} />
+          <Route path="/user/create" element={<div>UTWORZ ADMINA</div>} />
+        </Routes>
+      </div>
+      ;
+    </Router>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import { useForm } from "react-hook-form";
+
+export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return <form></form>;
+}
